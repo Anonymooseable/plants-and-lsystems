3cf3c897b3fd863c5b507ac2bef1f67d6f2d0072
Bobbster <bobbsterman@gmail.com>
2014-04-21 17:12:51 +0200

    Added progrssive L-system py file

---
+++ progressive rules system.py
@@ -0,0 +1,7 @@
+#The first structurally randomly generated L-system will be the 
+#"progressive_rules" for the represented L-system. It will take some kind of
+#noise and then shape that noise into a structure (using it's own, randomly 
+#generated rules, the "seed_rules") which will be translated into the
+#"progressive_rules".
+#I am effectively trying to create an L-system whose rules are being
+#progressively being generated.
