cd7bb7335fa6622037b350ceb8102118cde4372f
Bobbster <bobbsterman@gmail.com>
2014-05-15 20:42:59 +0200

    Further work on Treesystem
    Removed all traces of Fernsystem from Tree and replaced randrange with
    gaussian distrib for angle turns.

--- lsys/example/tree.py
+++ lsys/example/tree.py
@@ -15,8 +15,8 @@ class TreeSystem(core.System):
                       "push", "right", "smaller", "branch", "pop"], 2),
                     )
             "fw": core.StochasticRule(
-                    (["fw","fw","fw"],math.floor(40/self.actions)+1)
-                    (["fw","fw"],math.floor(80/self.actions)+1)
+                    (["fw","fw","fw"],math.floor(40/len(self.actions))+1)
+                    (["fw","fw"],math.floor(80/len(self.actions))+1)
                     (["fw"],6)
         }, actions={
             "fw": self.fw,
@@ -24,41 +24,25 @@ class TreeSystem(core.System):
             "pop": self.pop,
             "left": self.left,
             "right": self.right,
-            "smaller": self.smaller,
         }, axiom=["branch"], **kwargs)
-        self.length_stack = [140]
 
     def fw(self):
-        self.renderer.draw_segment(self.length_stack[-1])
-        self.renderer.turn(-1)
-
-    def branch(self):
-        self.renderer.draw_segment(self.length_stack[-1] * 4)
-
-    def smaller(self):
-        self.length_stack[-1] /= (math.log(len(self.length_stack),math.e)/2.8)+1
-        self.length_stack[-1] += random.random()*(self.length_stack[-1]/2.1)-(self.length_stack[-1]/4.2)
+        self.renderer.draw_segment(20)
 
     def left(self):
-        self.renderer.turn(random.randrange(20,40))
+        self.renderer.turn(random.gauss(20,6.5))
 
     def right(self):
-        self.renderer.turn(-1*random.randrange(20,40))
+        self.renderer.turn(random.gauss(-20,6.5))
 
     def push(self):
         self.renderer.push ()
-        self.length_stack.append(self.length_stack[-1])
 
     def pop(self):
         self.renderer.pop()
-        self.renderer.turn(random.randrange(0,30)-15) # this makes the angle waver inside a branch
-                                                      # i.e. at every split the angles changes a bit
-        self.length_stack.pop()
-
-    def rotz(self):
-        self.renderer.rotz(1)
-
-
+        self.renderer.turn(random.gauss(0,5)) #cela fait varier l'angle
+                                             #dans une branche
+                         
 def main():
     #mode = "opengl"
     mode = "turtle"
@@ -71,8 +55,8 @@ def main():
     elif mode == "turtle":
         from lsys.render.turtle_renderer import TurtleRenderer
         r = TurtleRenderer()
-    s = FernSystem(renderer=r)
-    s.construct(depth=8, debug=False)
+    s = TreeSystem(renderer=r)
+    s.construct(, debug=False)
     s.render()
 
 if __name__ == "__main__":
