677c6e75b4f756de3c2fd37ab9bac88994bb669a
Linus <anonymouse2048@gmail.com>
2014-05-15 22:51:45 +0200

    Update stochastic rule class to work in new structure

--- lsys/core/stochastic_rule.py
+++ lsys/core/stochastic_rule.py
@@ -1,14 +1,15 @@
 import random
+import bisect
+import itertools
 
 class StochasticRule:
     def __init__(self, *output_sets):
         self.output_sets = output_sets
         self.sum = sum([s[1] for s in output_sets])
 
-    def __iter__(self):
-        r = random.randrange(self.sum + 1)
-        subtotal = 0
-        for s, factor in self.output_sets:
-            subtotal += factor
-            if r <= subtotal:
-                return iter(s)
+    def __call__(self):
+        # Shamelessly stolen from the docs (random module)
+        choices, weights = zip(*self.output_sets)
+        cumdist = list(itertools.accumulate(weights))
+        x = random.random() * cumdist[-1]
+        return choices[bisect.bisect(cumdist, x)]
