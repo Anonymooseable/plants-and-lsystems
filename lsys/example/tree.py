12f3514be3c408549ba2b1b28c98f1be284a3163
Linus <anonymouse2048@gmail.com>
2014-05-15 23:40:33 +0200

    Old fixed tree.py

--- lsys/example/tree.py
+++ lsys/example/tree.py
@@ -7,72 +7,53 @@ class TreeSystem(core.System):
     def __init__(self, *args, **kwargs):
         super().__init__(rules={
             "branch": core.StochasticRule(
-                    (["fw", "push", "smaller", "branch","pop"], 3),
+                    (["fw", "push", "smaller", "branch","pop"], 2),
                     (["fw", "push", "left", "smaller", "branch","pop",
-                      "push", "right", "smaller", "branch", "pop"], 2),
+                      "push", "right", "smaller", "branch", "pop"], 3),
                     (["fw", "push", "left", "smaller", "branch","pop",
                       "push", "smaller", "branch","pop",
                       "push", "right", "smaller", "branch", "pop"], 2),
-                    )
-            "fw": core.StochasticRule(
-                    (["fw","fw","fw"],math.floor(40/self.actions)+1)
-                    (["fw","fw"],math.floor(80/self.actions)+1)
-                    (["fw"],6)
+                    ),
+            "fw": ["fw","fw"]
         }, actions={
             "fw": self.fw,
             "push": self.push,
             "pop": self.pop,
             "left": self.left,
             "right": self.right,
-            "smaller": self.smaller,
         }, axiom=["branch"], **kwargs)
-        self.length_stack = [140]
 
     def fw(self):
-        self.renderer.draw_segment(self.length_stack[-1])
-        self.renderer.turn(-1)
-
-    def branch(self):
-        self.renderer.draw_segment(self.length_stack[-1] * 4)
-
-    def smaller(self):
-        self.length_stack[-1] /= (math.log(len(self.length_stack),math.e)/2.8)+1
-        self.length_stack[-1] += random.random()*(self.length_stack[-1]/2.1)-(self.length_stack[-1]/4.2)
+        self.renderer.draw_segment(20)
 
     def left(self):
-        self.renderer.turn(random.randrange(20,40))
+        self.renderer.turn(random.gauss(20,6.5))
 
     def right(self):
-        self.renderer.turn(-1*random.randrange(20,40))
+        self.renderer.turn(random.gauss(-20,6.5))
 
     def push(self):
         self.renderer.push ()
-        self.length_stack.append(self.length_stack[-1])
 
     def pop(self):
         self.renderer.pop()
-        self.renderer.turn(random.randrange(0,30)-15) # this makes the angle waver inside a branch
-                                                      # i.e. at every split the angles changes a bit
-        self.length_stack.pop()
-
-    def rotz(self):
-        self.renderer.rotz(1)
-
+        self.renderer.turn(random.gauss(0,5)) #cela fait varier l'angle
+                                             #dans une branche
 
 def main():
-    #mode = "opengl"
-    mode = "turtle"
+    mode = "opengl"
+    #mode = "turtle"
 
     if mode == "opengl":
         from lsys.render.gl_renderer import GLRenderer
-        r = GLRenderer(scale=4, size=(800, 800))
+        r = GLRenderer(scale=0.1, size=(800, 800))
         import OpenGL.GL as GL
         GL.glColor3f(0.2, 1.0, 0.0)
     elif mode == "turtle":
         from lsys.render.turtle_renderer import TurtleRenderer
         r = TurtleRenderer()
-    s = FernSystem(renderer=r)
-    s.construct(depth=8, debug=False)
+    s = TreeSystem(renderer=r)
+    s.construct(8, debug=False)
     s.render()
 
 if __name__ == "__main__":
